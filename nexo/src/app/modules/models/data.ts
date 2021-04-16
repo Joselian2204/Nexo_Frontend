1e4125e4d24338e8c483393a6b271f308dcc6a84
rodriguez-luis <pb.rndm@gmail.com>
2021-04-15 22:29:03 -0400

    add data model

---
+++ nexo/src/app/modules/models/data.ts
@@ -0,0 +1,10 @@
+export interface data{
+    "id":number,
+    "regionId":string,
+    "date":Date,
+    "cases":number,
+    "actives":number,
+    "deaths":number,
+    "recovered":number,
+    "vaccine":number
+}
